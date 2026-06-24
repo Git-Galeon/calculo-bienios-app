4a5580f2713427f74be8f1dcc3b83c206afcd220
Git-Galeon <fgalaz1994.au@gmail.com>
2026-06-24 15:47:22 -0400

    Update API_URL to new endpoint

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbysdpz0jxtm9go4qsayXreNQDie6k7f2lboO10KOTi1oY3xVKG0BwcLisav8XxD3KcF/exec"; 
+const API_URL = "https://script.google.com/macros/s/AKfycby1jX08m7dgL5K-P9ItmkkLOeLUrle_fwHkiXdgsMnA3kVkW5MBMFJuYNEkGwylfPa9/exec"; 
 const TOKEN = "A_gam3_n0n-7&7";
 
 let currentTab = 'dashboard';
